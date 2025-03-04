c13d38ffe506c0edeb2f3521b28d309cc34f386a
Jakob Kristensen <jakobk205@gmail.com>
2025-03-04 13:46:37 +0100

    Initial

---
+++ file.py
@@ -0,0 +1,17 @@
+import random
+
+answerInt = random.randint(1, 10)
+print(answerInt)
+
+guessInt = int(input("Guess a number between 1 and 10: "))
+
+for i in range(10):
+    if (guessInt == answerInt):
+        print((str(answerInt)) + " is correct!")
+        break
+    else:
+        guessInt = int(input("Try again: "))
+        if (answerInt > guessInt):
+            print("The answer is higher")
+        else:
+            print("The answer is lower")
