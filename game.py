bb081290ec7a876aaafc84071dd4d54d96425353
Jakob Kristensen <jakobk205@gmail.com>
2025-03-04 14:39:01 +0100

    Updated game display and added player rectangle

--- game.py
+++ game.py
@@ -1,17 +1,26 @@
 import pygame
+from pygame.locals import *
 
 pygame.init()
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Demo')
+screen.fill((200, 200, 200))
+
+player = pygame.Rect((300, 250, 50, 200))
+pygame.display.set_caption('Jakobs Game')
 
 run = True
 while run:
 
+    pygame.draw.rect(screen, (0, 255, 255), player)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    pygame.display.update()
 
 pygame.quit()
