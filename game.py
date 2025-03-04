b6c99174de8a1c1532bf6cf60f627f8440bc952f
Jakob Kristensen <jakobk205@gmail.com>
2025-03-04 14:58:25 +0100

    Redesigned game visuals with new shapes and colors

--- game.py
+++ game.py
@@ -1,21 +1,31 @@
 import pygame
-from pygame.locals import *
 
 pygame.init()
 
 SCREEN_WIDTH = 800
-SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
-
+SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 screen.fill((200, 200, 200))
 
-player = pygame.Rect((300, 250, 50, 200))
 pygame.display.set_caption('Jakobs Game')
 
 run = True
 while run:
 
-    pygame.draw.rect(screen, (0, 255, 255), player)
+    pygame.draw.circle(screen, (255, 255, 0), [400,300], 275)
+    pygame.draw.circle(screen, (255, 255, 0), [150,100], 50)
+    pygame.draw.circle(screen, (255, 255, 0), [650,100], 50)
+
+    pygame.draw.circle(screen, (0, 0, 0), [150,100], 10)
+    pygame.draw.circle(screen, (0, 0, 0), [650,100], 10)
+
+    pygame.draw.circle(screen, (0, 0, 0), [300,250], 25)
+    pygame.draw.circle(screen, (0, 0, 0), [500,250], 25)
+
+    pygame.draw.circle(screen, (255, 255, 255), [300,250], 10)
+    pygame.draw.circle(screen, (255, 255, 255), [500,250], 10)
+
+    pygame.draw.arc(screen, (255, 0, 0), [300, 325, 200, 150], 3.14, 6.28, 10)    
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
